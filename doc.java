749b351a86f196f6ae1ab75fd28d039736a52d7e
anaghu <129377733+anaghu@users.noreply.github.com>
2023-03-31 10:59:28 +0530

    Assignment 2 submitted
    Uploaded assignment 2 of java essential Bootcamp day 4

---
+++ doc.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+  
+public class doc {
+  
+    public static void main(String[] args)
+    {
+        int a = 62;
+        int b = 18;
+        
+        System.out.println("The sum is =" + Integer.sum(a, b));
+    }
+}
